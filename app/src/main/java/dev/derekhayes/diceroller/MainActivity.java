9c3a6cd9b9a3e2e2c6271f032d9ce162be950e5d
Derek Hayes <derek2099@gmail.com>
2024-07-16 18:47:16 -0500

    Add dice to MainActivity controller/fix layout

--- app/src/main/java/dev/derekhayes/diceroller/MainActivity.java
+++ app/src/main/java/dev/derekhayes/diceroller/MainActivity.java
@@ -1,14 +1,28 @@
 package dev.derekhayes.diceroller;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final int MAX_DICE = 3;
+
+    private int mVisibleDice;
+    private Dice[] mDice;
+    private ImageView[] mDiceImageViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,66 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mDice = new Dice[MAX_DICE];
+        for (int i = 0; i < MAX_DICE; i++) {
+            mDice[i] = new Dice(i + 1);
+        }
+
+        mDiceImageViews = new ImageView[MAX_DICE];
+        mDiceImageViews[0] = findViewById(R.id.dice1);
+        mDiceImageViews[1] = findViewById(R.id.dice2);
+        mDiceImageViews[2] = findViewById(R.id.dice3);
+
+        mVisibleDice = MAX_DICE;
+
+        showDice();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void showDice() {
+        for (int i = 0; i < mVisibleDice; i++) {
+            Drawable diceDrawable = ContextCompat.getDrawable(this, mDice[i].getImageId());
+            mDiceImageViews[i].setImageDrawable(diceDrawable);
+            mDiceImageViews[i].setContentDescription(Integer.toString(mDice[i].getNumber()));
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_one) {
+            changeDiceVisibility(1);
+            showDice();
+            return true;
+        }
+        else if (item.getItemId() == R.id.action_two) {
+            changeDiceVisibility(2);
+            showDice();
+            return true;
+        }
+        else if (item.getItemId() == R.id.action_three) {
+            changeDiceVisibility(3);
+            showDice();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void changeDiceVisibility(int numVisible) {
+        mVisibleDice = numVisible;
+
+        for (int i = 0; i < numVisible; i++) {
+            mDiceImageViews[i].setVisibility(View.VISIBLE);
+        }
+
+        for (int i = numVisible; i < MAX_DICE; i++) {
+            mDiceImageViews[i].setVisibility(View.GONE);
+        }
     }
 }
